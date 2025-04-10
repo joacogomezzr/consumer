a2e92a83b17a0a42e19a4d0061fad4ba9366062f
joacogomezzr <joaquingomezz199@gmail.com>
2025-04-10 10:58:05 -0600

    some changes

--- internal/handlers/log_handler.go
+++ internal/handlers/log_handler.go
@@ -7,11 +7,12 @@ import (
 	"net/http"
 )
 
-// HandleMessageAndSend procesa el mensaje y envía el ID a la API
+// HandleMessageAndSend procesa el mensaje y envía el ID del libro a la API de recomendaciones
 func HandleMessageAndSend(message []byte) {
 	// Parsear el mensaje recibido
 	var data struct {
-		ID int64 `json:"id"`
+		ID           int64 `json:"id"`
+		Recommendable bool  `json:"recommendable"`
 	}
 	err := json.Unmarshal(message, &data)
 	if err != nil {
@@ -19,11 +20,20 @@ func HandleMessageAndSend(message []byte) {
 		return
 	}
 
-	log.Printf("ID recibido: %d", data.ID)
+	log.Printf("ID recibido: %d, Recommendable: %t", data.ID, data.Recommendable)
 
-	// Construir la solicitud para la API
+	// Validar si el libro es Recommendable
+	if data.Recommendable {
+		log.Println("El libro es recommendable, enviando a la API de recomendaciones.")
+		sendToRecommendationsAPI(data.ID)
+	} else {
+		log.Println("El libro no es recommendable, no se enviará a la API.")
+	}
+}
+
+func sendToRecommendationsAPI(bookID int64) {
 	apiURL := "http://127.0.0.1:4000/v1/recomendaciones/"
-	payload := map[string]int64{"id": data.ID}
+	payload := map[string]int64{"idBook": bookID}
 	body, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error creando el payload para la API: %v", err)
